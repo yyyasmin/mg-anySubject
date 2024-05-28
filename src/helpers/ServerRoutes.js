d192c0d208e9be2942c860676d0a9eec94c8596b
unknown <you@example.com>
2024-05-28 22:34:34 +0300

    SERVER URL

--- src/helpers/ServerRoutes.js
+++ src/helpers/ServerRoutes.js
@@ -1,5 +1,5 @@
 export const PROXY_URL = "http://localhost:5000";
-export const RENDER_PROXY_URL = "https://mg-young-kids-production.up.railway.app";  // mg-young_kids DEPLOY
+export const RENDER_PROXY_URL = "https://mg-anysubject-production.up.railway.app";  // anySubject DEPLOY URL
 
 export const CHOSEN_PROXY_URL = RENDER_PROXY_URL;
 //export const CHOSEN_PROXY_URL = PROXY_URL;
